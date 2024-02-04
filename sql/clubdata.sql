6038b5b567986f4e746df62d87bf2973f41d1e8b
vlabliuk <endround@yandex.ru>
2024-02-04 21:13:37 +0000

    feat: added sql queries

--- sql/clubdata.sql
+++ sql/clubdata.sql
@@ -4244,39 +4244,6 @@ ALTER TABLE ONLY members
 -- PostgreSQL database dump complete
 --
 
-CREATE INDEX "bookings.memid_facid"
-    ON cd.bookings
-    USING btree
-    (memid, facid);
-
-CREATE INDEX "bookings.facid_memid"
-    ON cd.bookings
-    USING btree
-    (facid, memid);
-
-CREATE INDEX "bookings.facid_starttime"
-    ON cd.bookings
-    USING btree
-    (facid, starttime);
-
-CREATE INDEX "bookings.memid_starttime"
-    ON cd.bookings
-    USING btree
-    (memid, starttime);
-
-CREATE INDEX "bookings.starttime"
-    ON cd.bookings
-    USING btree
-    (starttime);
-
-CREATE INDEX "members.joindate"
-    ON cd.members
-    USING btree
-    (joindate);
-
-CREATE INDEX "members.recommendedby"
-    ON cd.members
-    USING btree
-    (recommendedby);
+cd
 
 ANALYZE;
---
+++ sql/queries.sql
@@ -0,0 +1,206 @@
+-- set cd as 1st search schema
+
+SET search_path TO cd;
+
+-- insert
+
+INSERT INTO facilities
+VALUES
+    (9, 'Spa', 20, 30, 100000, 800);
+
+-- select in insert
+
+INSERT INTO facilities
+VALUES
+    (
+        (
+            SELECT
+                max(facid)
+            FROM
+                cd.facilities
+        ) + 1,
+        'Spa',
+        20,
+        30,
+        100000,
+        800
+    );
+
+-- update
+
+UPDATE
+    facilities
+SET
+    initialoutlay = 10000
+WHERE
+    name = 'Tennis Court 2';
+
+-- update with calculation
+
+UPDATE
+    facilities
+SET
+    membercost = (
+        (
+            SELECT
+                membercost
+            FROM
+                cd.facilities
+            WHERE
+                name = 'Tennis Court 1'
+        ) * 1.1
+        ),
+    guestcost = (
+        (
+            SELECT
+                guestcost
+            FROM
+                cd.facilities
+            WHERE
+                name = 'Tennis Court 1'
+        ) * 1.1
+        )
+WHERE
+    name = 'Tennis Court 2';
+
+-- delete all
+
+TRUNCATE TABLE facilities CASCADE;
+
+-- delete condition
+DELETE FROM
+    cd.members
+WHERE
+    memid = 37;
+
+-- select with condition
+
+SELECT
+    f.facid,
+    f.name,
+    f.membercost,
+    f.monthlymaintenance
+FROM
+    facilities f
+WHERE
+    membercost > 0
+  AND membercost < (
+    (
+        SELECT
+            monthlymaintenance
+        FROM
+            facilities
+        WHERE
+            facid = f.facid
+    ) * 0.02
+    )
+
+-- Select with contains
+
+SELECT
+    *
+FROM
+    facilities
+WHERE
+    name LIKE '%Tennis%';
+
+-- Select two without OR
+
+SELECT
+    *
+FROM
+    facilities
+WHERE
+    facid = 1
+UNION
+SELECT
+    *
+FROM
+    facilities
+WHERE
+    facid = 5;
+
+-- Compare timestamp
+
+SELECT
+    memid,
+    surname,
+    firstname,
+    joindate
+FROM
+    members
+WHERE
+    DATE(joindate) >= '2012-09-01';
+
+
+-- Union members surnames and facilities names
+
+SELECT
+    surname
+FROM
+    members
+UNION
+SELECT
+    name
+FROM
+    facilities;
+
+-- Select with join and condition
+
+SELECT
+    starttime
+FROM
+    members m
+        JOIN bookings b ON m.memid = b.memid
+WHERE
+    surname = 'Farrell'
+  AND firstname = 'David';
+
+-- Join with conditions
+
+SELECT
+    b.starttime AS start,
+    f.name
+FROM
+    facilities f
+        JOIN bookings b ON f.facid = b.facid
+WHERE
+    DATE(b.starttime) = '2012-09-21'
+  AND name LIKE 'Tennis%'
+ORDER BY
+    b.starttime;
+
+-- Select with same table FK
+
+SELECT
+    m.firstname AS memfname,
+    m.surname AS memsname,
+    rm.firstname AS recfname,
+    rm.surname AS recsname
+FROM
+    members m
+        LEFT OUTER JOIN members rm ON rm.memid = m.recommendedby
+ORDER BY
+    (m.surname, m.firstname);
+
+-- Select with recommendedby
+
+SELECT
+    DISTINCT rm.firstname AS firstname,
+             rm.surname AS surname
+FROM
+    cd.members m
+        JOIN cd.members rm ON rm.memid = m.recommendedby
+ORDER BY
+    surname,
+    firstname;
+
+-- select without join
+
+SELECT DISTINCT m.firstname || ' ' ||
+                m.surname AS member, (SELECT rm.firstname || ' ' ||
+                                             rm.surname AS recommender FROM cd.members rm WHERE rm.memid = m.recommendedby )
+FROM
+    cd.members m
+ORDER BY member;
+
