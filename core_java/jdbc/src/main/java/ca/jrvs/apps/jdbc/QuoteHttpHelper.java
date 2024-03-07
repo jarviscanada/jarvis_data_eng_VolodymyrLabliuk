535912e39fcefe6d24bc91a21e539ae403d9ef23
vlabliuk <endround@yandex.ru>
2024-03-07 18:34:15 +0000

    fix: http call changed to OkHttpClient

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/QuoteHttpHelper.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/QuoteHttpHelper.java
@@ -4,26 +4,20 @@ package ca.jrvs.apps.jdbc;
 import okhttp3.OkHttpClient;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.sql.Timestamp;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class QuoteHttpHelper {
 
     private String apiKey = "4edeebbaf9msh53822f7bf795ee1p1d968djsn9190c9150d97";
-    private OkHttpClient client;
+    private OkHttpClient client = new OkHttpClient();
 
     /**
      * Fetch latest quote data from Alpha Vantage endpoint
@@ -33,16 +27,18 @@ public class QuoteHttpHelper {
      */
     public Quote fetchQuoteInfo(String symbol) throws IllegalArgumentException {
 
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://alpha-vantage.p.rapidapi.com/query?function=GLOBAL_QUOTE&symbol="+symbol+"&datatype=json"))
+        Request request = new Request.Builder()
+                .url("https://alpha-vantage.p.rapidapi.com/query?function=GLOBAL_QUOTE&symbol="+symbol+"&datatype=json")
                 .header("X-RapidAPI-Key", apiKey)
                 .header("X-RapidAPI-Host", "alpha-vantage.p.rapidapi.com")
-                .method("GET", HttpRequest.BodyPublishers.noBody())
+                .get()
                 .build();
-        try {
-            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response);
+            }
             ObjectMapper objectMapper = new ObjectMapper();
-            JsonNode jsonNode = objectMapper.readTree(response.body());
+            JsonNode jsonNode = objectMapper.readTree(response.body().string());
             String jsonToParse = "";
 
             if (jsonNode.has("Global Quote")) {
@@ -58,8 +54,6 @@ public class QuoteHttpHelper {
             }
 
             return objectMapper.readValue(jsonToParse, Quote.class);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         } catch (JsonMappingException e) {
             e.printStackTrace();
         } catch (JsonProcessingException e) {
